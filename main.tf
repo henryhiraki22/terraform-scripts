45dcdab9acb260430ff5b19e8988a3434eb7f1f9
henryhiraki22 <henry.hiraki@gmail.com>
2021-09-10 12:08:16 -0300

    update files

--- main.tf
+++ main.tf
@@ -1,17 +1,16 @@
 resource "aws_instance" "k8s-nodes" {
   ami       = "ami-054a31f1b3bf90920"
   instance_type = "t2.micro"
-  count = 3
+  count = 1
 
   tags = {
-       Name = "Server ${count.index}"
+       Name = "Cluster ${count.index}"
   }  
 }
 
 resource "aws_security_group" "web-node" {
   name = "web-node"
   description = "Web Security Group"
-
     ingress {
         from_port = 80
         to_port = 80
@@ -33,20 +32,21 @@ resource "aws_security_group" "web-node" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
-    egress  {
-        from_port = 0
-        to_port = 0 
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
     tags = {
         type = "k8s-security-group"
     }
 }
 
 resource "aws_network_interface_sg_attachment" "k8s_attachment" {
-    count = 3
+    count = 1
     security_group_id = aws_security_group.web-node.id
     network_interface_id = aws_instance.k8s-nodes[count.index].primary_network_interface_id
 }
+
+
+
+
+
+
+
+
